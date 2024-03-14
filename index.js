21da13a596225e69fde642c56517e971438f6eb9
alisha99699l <144793236+alisha99699l@users.noreply.github.com>
2024-03-14 18:02:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var numberOfDrum=document.querySelectorAll(".drum").length;
+for(i=0; i< numberOfDrum; i++)
+{
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function()
+{
+   
+   this.style.color="white";
+   var buttonInner= this.innerHTML;
+   makesound(buttonInner);
+   buttonAnimation(buttonInner);
+ 
+});
+}
+
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+   
+});
+function makesound(key){
+    switch(key){
+        case 'w':
+            var audio= new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio= new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+    
+        case 's':
+            var audio= new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio= new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+    
+        case 'j':
+            var audio= new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+    
+        case 'k':
+            var audio= new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+    
+         case 'l':
+            var audio= new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+           
+        default:
+            console.log(buttonInner);
+     }
+
+
+}
+
+
+
+
